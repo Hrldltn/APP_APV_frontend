89bd14640f04a20769e6d2a444de91fa9ff52a45
Harold Leyton <109939979+Hrldltn@users.noreply.github.com>
2022-08-29 02:05:57 -0400

    Update Login.jsx

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
     <>
         <div>
              <h1 className="text-sky-400 font-black text-4xl md:text-6xl">inicia Sesión y Administra tus <span className="text-black text-4xl md:text-6xl">Pacientes</span> </h1>
-             <img className="hidden md:block"src="../assets/img/perros.png" alt="perros-img"/>
+             <img className="hidden md:block" src="../assets/img/perros.png" alt="perros-img"/>
         </div>
         <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl'>
             <img src="../assets/img/gato.jpg" alt="gato-img"/>
