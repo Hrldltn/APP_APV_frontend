9971e072ba21cb8046036930ff4875275f2c7092
Harold Leyton <109939979+Hrldltn@users.noreply.github.com>
2022-08-29 01:39:16 -0400

    Update Registrar.jsx

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,7 +1,7 @@
 import{useState} from 'react'
 import {Link} from 'react-router-dom'
 import clienteAxios from '../config/axios'
-import Alerta from '../Components/Alerta'
+import Alerta from '../Components/Alerta.js'
 
 const Registrar = () => {
   const [nombre,setNombre] = useState('')
